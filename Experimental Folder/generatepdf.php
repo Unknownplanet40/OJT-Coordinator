fa23dc39779b492609cad0f09946ed1c80c63e3f
Ryan James Capadocia <ryanjamesc4@gmail.com>
2023-07-08 21:36:26 +0800

    Caps Bugs Fix in AdminEvent Page

--- Experimental Folder/generatepdf.php
+++ Experimental Folder/generatepdf.php
@@ -1,6 +1,6 @@
 <?php
 // Include the TCPDF library
-@require_once('../Components/TCPDF/tcpdf.php');
+@require_once('./TCPDF/tcpdf.php');
 
 // Create a new PDF document
 $pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8');
@@ -19,7 +19,7 @@ ob_start();
 //V1
 //include('../Components/PlacementFormPDF.html');
 //V2
-include('../Components/PlacementFormPDF copy.html');
+@include_once('./PlacementFormPDF.html');
 
 // Get the captured output
 $html = ob_get_clean();
@@ -30,14 +30,5 @@ $pdf->AddPage();
 // Convert the HTML into PDF
 $pdf->writeHTML($html, true, false, true, false, '');
 // Output the PDF as a file named "PlacementForm.pdf" and force download
-function random_string($length) {
-    $key = '';
-    $keys = array_merge(range(0, 9), range('a', 'z'));
- 
-    for ($i = 0; $i < $length; $i++) {
-        $key .= $keys[array_rand($keys)];
-    }
- 
-    return $key;
-}
-$pdf->Output('PlacementForm'.random_string(5).'.pdf', 'D');
+$pdf->Output('PlacementForm.pdf', 'D');
+?>
